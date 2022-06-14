c808f2bf9a869d322b8008d74018e08b478a2e9f
Tossavainen Teemu <teemu.tossavainen@hiq.fi>
2022-06-14 09:44:39 +0300

    UnitTest fix

--- Frends.Community.Multipart.SendMultipartRequest.Tests/UnitTests.cs
+++ Frends.Community.Multipart.SendMultipartRequest.Tests/UnitTests.cs
@@ -29,21 +29,11 @@ namespace Frends.Community.Multipart.SendMultipartRequest.Tests
 
             var textData = new TextData[]
             {
-                new TextData
-                {
-                    Key = "attachments",
-                    Value = @"[{""title"": ""Random"", ""image_url"": ""https://upload.wikimedia.org/wikipedia/en/0/0b/Postman-Pat.jpg""}]"
-                },
-                new TextData
-                {
-                    Key = "channel",
-                    Value = "G01QH4ES8SV"
-                }
             };
 
             var input = new SendInput
             {
-                Url = @"https://slack.com/api/chat.postMessage", // https://slack.com/api/chat.postMessage
+                Url = "",
                 FilePaths = files,
                 Headers = headers,
                 Bodys = bodies,
@@ -53,7 +43,7 @@ namespace Frends.Community.Multipart.SendMultipartRequest.Tests
             var options = new SendOptions()
             {
                 Authentication = AuthenticationMethod.OAuth2,
-                BearerToken = "xoxb-1816171944434-1826655541764-UcWHT5s4O9BE50SXNSai4oXs",
+                BearerToken = "",
             };
 
             var result = await MultipartTasks.SendMultipartRequest(input, options, new CancellationToken());
